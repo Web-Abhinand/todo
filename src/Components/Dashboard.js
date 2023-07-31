6bad57625583f810619f1eefa90caa0b3349522a
Abhinand <websollabsabhinand@gmail.com>
2023-07-31 14:42:08 +0530

    to_do edit functionality added

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import { Button, Card, Alert, Form,Container } from 'react-bootstrap'
+import { Button, Card, Alert, Form, Container } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
 import { Link, useNavigate } from 'react-router-dom'
 import { db } from '../firebase'
-import { collection, doc, setDoc, getDoc, getDocs, query, where, deleteDoc,addDoc } from "firebase/firestore";
+import { collection, doc, setDoc, getDoc, getDocs, query, where, deleteDoc, addDoc, updateDoc } from "firebase/firestore";
 import deleteIcon from '../Assets/delete_black_24dp.svg';
 import accountCircle from '../Assets/account_circle_black_24dp.svg';
 import editIcon from '../Assets/edit_black_24dp.svg';
+import { hover } from '@testing-library/user-event/dist/hover'
 function Dashboard() {
   const { currentUser, logout } = useAuth()
   const [error, setError] = useState('')
@@ -14,6 +15,9 @@ function Dashboard() {
   const [todo, setTodo] = useState([]);
   const [display, setDisplay] = useState(true);
   const [count, setCount] = useState(0);
+  const [editedName, setEditedName] = useState('');
+const [editedTodo, setEditedTodo] = useState('');
+const [editIndex, setEditIndex] = useState(-1); // -1 indicates no editing is currently happening
 
   useEffect(() => {
     handleDisplayData();
@@ -30,7 +34,7 @@ function Dashboard() {
   // code to enter data from form to firebase database
 
 
-  
+
   async function handleSubmit(event) {
     setCount(count => count + 1);
 
@@ -40,11 +44,31 @@ function Dashboard() {
 
     try {
       console.log(event.target.elements);
+
+      // Get the reference to the existing document
       const todoRef = doc(collection(db, currentUser.email), "ToDoDetails");
-      await setDoc(todoRef, {
-        name: name,
-        todo: todo,
-      });
+      const todoDoc = await getDoc(todoRef);
+
+      // Check if the document exists
+      if (todoDoc.exists()) {
+        // If the document exists, update it by adding the new task to the tasks array
+        const existingData = todoDoc.data();
+        const existingTasks = existingData.tasks || [];
+
+        // Add the new task to the tasks array
+        existingTasks.push({ name, todo });
+
+        // Update the document with the new tasks array
+        await updateDoc(todoRef, {
+          tasks: existingTasks,
+        });
+      } else {
+        // If the document doesn't exist, create it with the first task in the tasks array
+        await setDoc(todoRef, {
+          tasks: [{ name, todo }],
+        });
+      }
+
       alert('Task added successfully!');
     } catch (error) {
       alert(error.message);
@@ -52,18 +76,25 @@ function Dashboard() {
   }
 
   async function handleDisplayData() {
+    console.log('Displaying data...  fn called');
     try {
       const docRef = doc(db, currentUser.email, "ToDoDetails");
       const docSnapshot = await getDoc(docRef);
 
       if (docSnapshot.exists()) {
         const todoDetails = docSnapshot.data();
-        const name = todoDetails.name;
-        const todoo = todoDetails.todo;
-        setTodo([...todo, { name, todoo }]);
-        console.log('Name:', name);
-        console.log('Todo:', todoo);
-        console.log(todo, 'todo');
+        const tasks = todoDetails.tasks || []; // Access the "tasks" array from the document data
+
+        // Clear the existing tasks before adding the new ones
+        setTodo([]);
+
+        // Iterate over each task and add it to the state
+        tasks.forEach((task) => {
+          const { name, todo } = task;
+          setTodo((prevTodo) => [...prevTodo, { name, todo }]);
+        });
+
+        console.log('Tasks:', tasks);
       } else {
         setTodo([]);
         console.log("No such document!");
@@ -71,29 +102,62 @@ function Dashboard() {
     } catch (error) {
       console.log(error);
     }
+    //code for reloading the page
+
   }
 
 
-  async function handleDelete() {
+  async function handleDelete(index) {
     try {
-      const docRef = doc(db, currentUser.email, "ToDoDetails");
+      const updatedTodo = [...todo]; // Create a copy of the todo array
+      updatedTodo.splice(index, 1); // Remove the task at the specified index
+      setTodo(updatedTodo); // Update the state with the updated array
 
-      await deleteDoc(docRef);
+      // You may choose to delete the task from Firebase Firestore here if needed
 
-      window.alert('ToDoDetails deleted successfully');
-      // setDisplay(false);
-      handleDisplayData();
+      window.alert('Task deleted successfully');
     } catch (error) {
-      console.error('Error deleting ToDoDetails:', error);
+      console.error('Error deleting task:', error);
 
       // Display error alert
-      window.alert('An error occurred while deleting ToDoDetails');
+      window.alert('An error occurred while deleting task');
     }
+  }
+
+  function handleEdit(index) {
+    // Set the edited task name and task to the current values of the task at the specified index
+    setEditedName(todo[index].name);
+    setEditedTodo(todo[index].todoo);
+    setEditIndex(index); // Set the index of the task we are editing
+  }
+  
+  function handleSave(index) {
+    // Create a copy of the todo array
+    const updatedTodo = [...todo];
+  
+    // Update the task name and task at the specified index with the edited values
+    updatedTodo[index].name = editedName;
+    updatedTodo[index].todoo = editedTodo;
+  
+    // Update the state with the updated array
+    setTodo(updatedTodo);
+  
+    // Reset the edit state
+    setEditedName('');
+    setEditedTodo('');
+    setEditIndex(-1);
+  }
+  
+  function handleCancel() {
+    // Reset the edit state
+    setEditedName('');
+    setEditedTodo('');
+    setEditIndex(-1);
   }
 
   return (
     <>
-      <header style={{ width: '100%', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', display: 'flex', justifyContent: 'space-between'}}>
+      <header style={{ width: '100%', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', display: 'flex', justifyContent: 'space-between' }}>
         <div style={{ width: '20%', marginLeft: '1rem' }}>
           <h1>ToDo</h1>
         </div>
@@ -103,17 +167,17 @@ function Dashboard() {
           </div>
         </div>
       </header>
-      <Container fluid className="d-flex justify-content-center" style={{ backgroundColor: '#f0f0f0',padding: '5rem'}}>
+      <Container fluid className="d-flex justify-content-center" style={{ backgroundColor: '#f0f0f0', padding: '5rem' }}>
         <Card style={{ width: '50%' }} >
           <Card.Body className="text-center">
-            <img src={accountCircle} style={{ width: '10%', mx: 'auto' }} className="mx-auto"/>
+            <img src={accountCircle} style={{ width: '10%', mx: 'auto' }} className="mx-auto" />
             <h2 className='text-center mb-4'>Profile</h2>
             {error && <Alert variant='danger'>{error}</Alert>}
             <p className='text-center mb-4'><strong>Email:</strong> {currentUser.email}</p>
           </Card.Body>
         </Card>
       </Container>
-      <Container fluid className="d-flex justify-content-center" style={{padding: '5rem'}}>
+      <Container fluid className="d-flex justify-content-center" style={{ padding: '5rem' }}>
         <Form onSubmit={handleSubmit}>
           <Form.Group id='name'>
             <Form.Label>Task Name</Form.Label>
@@ -129,23 +193,48 @@ function Dashboard() {
       {/* <div className='text-center mt-2'>
         <Button onClick={handleDisplayData}>Display Task</Button>
       </div> */}
-      <div className='hello'>
-        {todo.map((item) => {
-          return (
-            <>
-            <Container style={{ width: '50%' }}>
-              <Card style={{ marginTop: '1rem', marginBottom: '1rem' }}>
-                <div style={{ padding: '1rem' }}>
-                  <p>Task Name: {item.name}</p>
-                  <p style={{ marginBottom: '5px' }}>Task: {item.todoo}</p>
-                  <button onClick={handleDelete}><img src={deleteIcon} alt='delete_icon'/></button>
-                  <button><img src={editIcon} alt='edit_icon'/></button>
-                </div>
-              </Card>
-            </Container>
-            </>
-          )
-        })}
+      <div className='hello' style={{ backgroundColor: '#f0f0f0', padding: '5rem' }}>
+      <Container style={{ width: '50%' }}>
+        {todo.map((item, index) => (
+          <div key={index} className='card' style={{ marginTop: '1rem', marginBottom: '1rem' }}>
+            {editIndex === index ? (
+              // Check if we are editing this task
+              <div style={{ padding: '1rem' }}>
+                <input
+                  type="text"
+                  value={editedName}
+                  onChange={(e) => setEditedName(e.target.value)}
+                />
+                <input
+                  type="text"
+                  value={editedTodo}
+                  onChange={(e) => setEditedTodo(e.target.value)}
+                />
+                <button onClick={() => handleSave(index)}>Save</button>
+                <button onClick={() => handleCancel()}>Cancel</button>
+              </div>
+            ) : (
+              // Display task details
+              <div style={{ padding: '1rem' }}>
+                <p>Task Name: {item.name}</p>
+                <p style={{ marginBottom: '5px' }}>Task: {item.todo}</p>
+                <button
+                  onClick={() => handleEdit(index)}
+                  style={{ border: 'none', background: 'none', color: 'inherit', cursor: 'pointer' }}
+                >
+                  <img src={editIcon} alt="edit_icon" />
+                </button>
+                <button
+                  onClick={() => handleDelete(index)}
+                  style={{ border: 'none', background: 'none', color: 'inherit', cursor: 'pointer', transition: 'background-color 0.3s ease' }}
+                >
+                  <img src={deleteIcon} alt="delete_icon" />
+                </button>
+              </div>
+            )}
+          </div>
+        ))}
+      </Container>
       </div>
     </>
   )
